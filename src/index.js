543701f0d5abd181d59d87c6ee7e4a7a574a412f
beygee <doug0476@naver.com>
2019-12-20 07:23:09 +0900

    스키마 업데이트

--- src/index.js
+++ src/index.js
@@ -1,38 +1,27 @@
 const { GraphQLServer } = require('graphql-yoga')
-
-let links = [
-  {
-    id: 'link-0',
-    url: 'www.howtographql.com',
-    description: 'Fullstack tutorial for GraphQL'
-  }
-]
-let idCount = links.length
+const { prisma } = require('./generated/prisma-client')
 
 const resolvers = {
   Query: {
     info: () => 'This is the API of a hackernews clone',
-    feed: () => links
-  },
-  Link: {
-    id: parent => parent.id
+    feed: (parent, args, { prisma }, info) => {
+      return prisma.links()
+    }
   },
   Mutation: {
-    post: (parent, args) => {
-      const link = {
-        id: `link-${idCount++}`,
-        ...args
-      }
-
-      links.push(link)
-      return link
+    post: (parent, args, { prisma }) => {
+      return prisma.createLink({
+        url: args.url,
+        description: args.description
+      })
     }
   }
 }
 
 const server = new GraphQLServer({
   typeDefs: './src/schema.graphql',
-  resolvers
+  resolvers,
+  context: { prisma }
 })
 
 server.start(() => console.log(`http://localhost:4000 에서 서버 가동중`))
